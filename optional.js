ca663bfc1f458ac2062b228ffe1e2532737e7c81
Sambasivarao <samba.pendela@gmail.com>
2021-12-29 21:52:48 +0530

    code

---
+++ optional.js
@@ -0,0 +1,17 @@
+/*
+function fun_one(arg1?:string,arg2?:string,arg3?:string){
+    console.log( arg1, arg2, arg3 );
+}
+
+fun_one();                              //undefined undefined undefined
+fun_one("hello_1")                      //hello_1 undefined undefined
+fun_one(undefined,undefined,undefined); //undefined undefined undefined
+fun_one(null,null,null);                //null null null
+*/
+function fun_one(arg1, arg2) {
+    console.log(arg1, arg2);
+}
+//fun_one();                        //Expected 1-2 arguments, but got 0.
+//fun_one("Hello_1");               //Hello_1 undefined
+//fun_one(undefined,undefined);     //undefined undefined
+fun_one(null, null); //
